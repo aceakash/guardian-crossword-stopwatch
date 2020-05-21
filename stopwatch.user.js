406d40b010fffe8479fcc567b242d3bb3b9e7325
Akash <akash@pop-os.localdomain>
2020-05-21 20:56:13 +0100

    latest

--- stopwatch.user.js
+++ stopwatch.user.js
@@ -19,6 +19,8 @@
         }
     }, 100)
 
+    let startTime = null
+
     function isReady() {
         const el = document.querySelector('.crossword__clue__text')
         return el != null && el.textContent != null && el.textContent.length != null && el.textContent.length > 0
@@ -29,9 +31,9 @@
         watchNode.innerHTML = `
 <div class="display">00:00</div>
 <div>
-  <button>Start</button>
-  <button>Stop</button>
-  <button>Reset</button>
+  <button class="start">Start</button>
+  <button class="stop">Stop</button>
+  <button class="reset">Reset</button>
 </div>
 `
 
@@ -39,6 +41,38 @@
         const grid = document.querySelector('.content__main')
         watchNode = container.insertBefore(watchNode, grid)
         watchNode.style.paddingLeft = "8.25rem"
+
+        document.querySelector('button.start').onclick = start
+        document.querySelector('button.stop').onclick = stop
+        document.querySelector('button.reset').onclick = reset
+    }
+
+    function start() {
+        if (startTime != null) {
+          return
+        }
+        startTime = new Date()
+    }
+
+    function stop() {
+        if (startTime == null) {
+          return
+        }
+        let now = new Date()
+        document.querySelector('.display').innerText = (now-startTime)
+        startTime = null
+    }
+
+    function elapsedTime() {
+        if (startTime == null) {
+            return null
+        }
+        
+    }
+
+    function reset() {
+        startTime = null
+        document.querySelector('.display').innerText = '00:00'
     }
 
 
